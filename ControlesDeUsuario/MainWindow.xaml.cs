df17b0f35df78345b4b716f2e9e1f0d32bae8fd8
rech99 <rech_99@hotmail.com>
2019-09-26 08:41:58 -0700

    Agregar archivos de proyecto.

---
+++ ControlesDeUsuario/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlesDeUsuario
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            grdParametrosFigura.Children.Clear();
+            switch(cbfigura.SelectedIndex)
+            {
+                case 0:
+                    grdParametrosFigura.Children.Add(new ParametrosCirculo());
+                        break;
+                case 1:
+                    grdParametrosFigura.Children.Add(new ParametrosTriangulo());
+                    break;
+                case 2:
+                    grdParametrosFigura.Children.Add(new ParametrosRectangulo());
+                    break;
+                case 3:
+                    grdParametrosFigura.Children.Add(new ParametrosPentagono());
+                    break;
+                case 4:
+                    grdParametrosFigura.Children.Add(new ParametrosCuadarado());
+                    break;
+                case 5:
+                    grdParametrosFigura.Children.Add(new ParametrosTrapecio());
+                    break;
+               
+                default:
+                    break;
+            }
+        }
+    }
+}
